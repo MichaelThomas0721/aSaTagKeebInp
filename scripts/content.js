bf3d8a90710543bfb0bd4760e7caf7808d9d3bb3
MichaelThomas0721 <michaelthomas2107@gmail.com>
2024-10-10 08:12:22 -0400

    Added test sites

--- scripts/content.js
+++ scripts/content.js
@@ -3,30 +3,35 @@ let lastInp = null;
 
 // Function to handle mutations
 function handleMutations(mutationsList, observer) {
-    for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            let inp = document.getElementsByClassName("form-control")[0];
-            if (inp) {
-                if (inp == lastInp) return;
-                lastInp = inp;
-                inp.removeAttribute("readonly");
-                // observer.disconnect(); // Stop observing once element is found
-                inp.addEventListener('keydown', function (e) {
-                    if (e.repeat) return;
-                    if (e.key === 'Enter') {
-                        const enterBtn = document.querySelector("#virtualKeyboard > div > table > tbody > tr:nth-child(2) > td:nth-child(4) > button");
-                        if (!enterBtn) return;
-                        const mousedownEvent = new MouseEvent('mousedown', {
-                            bubbles: true,
-                            cancelable: true,
-                            view: window
-                        });
-                        enterBtn.dispatchEvent(mousedownEvent);
-                    }
-                })
-            }
-        }
-    }
+	for (let mutation of mutationsList) {
+		if (mutation.type === 'childList') {
+			let inps = document.getElementsByClassName("form-control");
+			inps = Array.from(inps);
+			if (!inps) return;
+			inps.forEach((inp) => {
+				if (inp) {
+					if (inp == lastInp) return;
+					lastInp = inp;
+					inp.removeAttribute("readonly");
+					// observer.disconnect(); // Stop observing once element is found
+					inp.addEventListener('keydown', function(e) {
+						if (e.repeat) return;
+						if (e.key === 'Enter') {
+							const enterBtn = document.querySelector("#virtualKeyboard > div > table > tbody > tr:nth-child(2) > td:nth-child(4) > button");
+							if (!enterBtn) return;
+							const mousedownEvent = new MouseEvent('mousedown', {
+								bubbles: true,
+								cancelable: true,
+								view: window
+							});
+							enterBtn.dispatchEvent(mousedownEvent);
+						}
+					})
+				}
+			})
+
+		}
+	}
 }
 
 // Options for the observer (which mutations to observe)
